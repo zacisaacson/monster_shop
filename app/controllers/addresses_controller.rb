ca1845882c4f0545cb0bb524e65ec804d2f1953a
Zac Isaacson <isaacson.zm@gmail.com>
2019-11-04 22:23:58 -0700

    Add edit and update actions to addresses controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,4 +2,17 @@ class AddressesController < ApplicationController
   def index
     @addresses = current_user.addresses
   end
+  def edit
+    @address = Address.find(params[:id])
+  end
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    if @address.save
+      redirect_to "/profile/addresses"
+    else
+      flash.now[:error] = @address.errors.full_messages
+      render :edit
+    end
+  end
 end
